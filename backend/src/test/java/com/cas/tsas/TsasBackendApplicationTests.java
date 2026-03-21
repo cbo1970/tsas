c6ba3661d96402c22ef768b0338d4fd1536d41a2
Christian Bonnhoff <cbo@zuehlke.com>
2026-03-21 15:34:00 +0100

    fix: replace TestNG @Test with JUnit Jupiter @Test in contextLoads test

--- backend/src/test/java/com/cas/tsas/TsasBackendApplicationTests.java
+++ backend/src/test/java/com/cas/tsas/TsasBackendApplicationTests.java
@@ -1,6 +1,6 @@
 package com.cas.tsas;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
