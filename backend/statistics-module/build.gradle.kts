c2f62daceeb409a69323b6188b6cdd06e0b9d712
Christian Bonnhoff <cbo@zuehlke.com>
2026-06-13 10:43:06 +0200

    build(statistics): depend on player-module for existence checks

--- backend/statistics-module/build.gradle.kts
+++ backend/statistics-module/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation(project(":common-module"))
     implementation(project(":match-module"))
+    implementation(project(":player-module"))
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
